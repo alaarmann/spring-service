bb59e6f011dd047be9a01f7aaae52a2c10f38677
Alexander Laarmann <alaarmann@gmx.net>
2018-01-27 06:15:44 +0100

    Created simple resource

---
+++ src/main/java/task/Task.java
@@ -0,0 +1,20 @@
+package task;
+
+public class Task {
+
+    private final long id;
+    private final String reference;
+
+    public Task(long id, String reference) {
+        this.id = id;
+        this.reference = reference;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+}
--- src/main/java/task/TaskController.java
+++ src/main/java/task/TaskController.java
@@ -1,18 +1,20 @@
 package task;
 
+
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class TaskController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String task() {
-        return "Here is the task service.";
+  private final AtomicLong counter = new AtomicLong();
+    @RequestMapping("/task")
+    public Task task(@RequestParam(value="reference", defaultValue="DefaultReference") String reference) {
+        return new Task(counter.incrementAndGet(), reference);
     }
 
     public static void main(String[] args) throws Exception {
